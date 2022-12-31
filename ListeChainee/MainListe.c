497b04f02fac7c2d9a362ffbc507225a1430b913
Ridouane <ridouaneaboubacarvisio8092@gmail.com>
2022-12-31 20:42:35 +0100

    feat(Liste):insererposition est ok

--- ListeChainee/MainListe.c
+++ ListeChainee/MainListe.c
@@ -3,7 +3,7 @@
 int main()
 {
 	system("clear");
-	TypeCellule *L=SaisieListeEnvers();
+	/*TypeCellule *L=SaisieListeEnvers();
 	L=InsererTete(L,0.1);
 	L=InsererQueue(L,0.2);
 	TypeCellule *L1;
@@ -13,5 +13,12 @@ int main()
 	Liberation(&L);
 	Liberation(&L1);
 	AfficherListe(L);
+	*/
+	TypeCellule *L2;
+	L2=InsererTete(L2,1.1);
+	L2=InsererTete(L2,2.2);
+	L2=InsererTete(L2,3.3);
+	L2=InsererPosition(L2,5,5);
+	AfficherListe(L2);
 	return 0;
 }
--- ListeChainee/SourceListe.c
+++ ListeChainee/SourceListe.c
@@ -66,8 +66,11 @@ TypeCellule *InsererQueue(TypeCellule *AncienneListe,TypeDonnee Donnee)
 }
 
 
-TypeCellule *SaisieListeEndroit()                  {                                                TypeCellule *Liste=ListeVide();                TypeDonnee elem;                                  char rep;                         printf("\nVous ne voulez pas, une liste tout simplement n'est ce pas?");
-	scanf("%s",&rep);                                         while(rep=='o')                                   {                                                   elem=SaisieDonnee();                         Liste=InsererQueue(Liste,elem);                                                  printf("\n%f A ETE INSERER EN TETE DE LISTE.\nVOUS VOULEZ CONTINUER A INSERER?",elem);
+TypeCellule *SaisieListeEndroit()                  {                                                TypeCellule *Liste=ListeVide();                
+	TypeDonnee elem;                                  char rep;                        
+	printf("\nVous ne voulez pas, une liste tout simplement n'est ce pas?");
+	scanf("%s",&rep);                                         while(rep=='o')                                   {                                                   elem=SaisieDonnee();                         Liste=InsererQueue(Liste,elem);      
+		printf("\n%f A ETE INSERER EN TETE DE LISTE.\nVOUS VOULEZ CONTINUER A INSERER?",elem);
 	scanf("%s",&rep);
 }                                       
 return Liste;                             
@@ -107,7 +110,33 @@ void Liberation(TypeCellule **pl)//pour avoir l'adresse de la tete
 }
 TypeCellule *InsererPosition(TypeCellule *AncienneListe,int Position,TypeDonnee Donnee)
 {
-	TypeCellule *nouv;
-	nouv=
+	TypeCellule *nouv,*ptr,*ptr1;
+	int posi=0;
+	nouv=(TypeCellule*)malloc(sizeof(TypeCellule));
+	nouv->Donnee=Donnee;
 	if(EstListeVide(AncienneListe))
+		AncienneListe=InsererTete(AncienneListe,Donnee);
+	else
+	{
+		ptr=AncienneListe;
+		while(ptr!=NULL && posi<Position-1)
+		{
+			ptr1=ptr;
+			ptr=ptr->Suivant;
+			posi++;
+		}
+		if(ptr==NULL)
+			printf("\nLA POSITION %d N'EXITE PAS DANS LA LISTE!",Position);
+		 else{
+			 ptr1->Suivant=nouv;
+			 nouv->Suivant=ptr;
+		 }
+	}
+	return AncienneListe;
+}
+
+
+
+
+
 		
